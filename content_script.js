7f7ca9490b9c4b6878c4daaad9ce640352af9745
Zachary <hello@zach.ie>
2017-01-10 21:28:48 +0100

    Minor code cleanup

--- content_script.js
+++ content_script.js
@@ -1,29 +1,25 @@
 var data = {};
 var last = {};
 var players = [];
-
 NUM_TURNS = 10;
 addedCityLabel = false
 
 function mode(array) {
   if(array.length == 0)
     return null;
-
   var modeMap = {};
   var maxEl = array[0], maxCount = 1;
-
   for(var i = 0; i < array.length; i++) {
     var el = array[i];
     if(modeMap[el] == null)
-        modeMap[el] = 1;
-      else
-        modeMap[el]++;  
-      if(modeMap[el] > maxCount){
-        maxEl = el;
-        maxCount = modeMap[el];
-      }
+      modeMap[el] = 1;
+    else
+      modeMap[el]++;  
+    if(modeMap[el] > maxCount){
+      maxEl = el;
+      maxCount = modeMap[el];
     }
-
+  }
   return maxEl;
 }
 
@@ -41,11 +37,14 @@ function updateCities(player, cities){
   var leaderboard = document.getElementById('game-leaderboard');
   var cities_cell = leaderboard.querySelectorAll('.cities-' + player);
   var player_row = cities_cell[0].parentElement;
+
+  // Add a flash if city count increases from zero
   if (cities_cell[0].innerHTML == "0" && cities > 0) {
     setTimeout(function(row, cl) { row.className = cl; }, 
       1000, player_row, player_row.className);
     player_row.className += " flash " + player;
   }
+
   cities_cell[0].innerHTML = cities;
 }
 
